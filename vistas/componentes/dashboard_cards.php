2ac4d197f090205372edaa57c53971cf08404ca0
valentina <vale.venica@gmail.com>
2020-08-09 19:03:50 -0300

    Agrego nuevo caso

--- vistas/componentes/dashboard_cards.php
+++ vistas/componentes/dashboard_cards.php
@@ -75,27 +75,44 @@ switch ($cardnum) {
         break;
     case 4: 
         $classcss = "bg-gradient-info";
-        $title = "Alumnos promedio en consultas";
+        $title = "CONSULTAS ACEPTADAS";
         $letter = "A";
-
-$sql = "
-/*
-              alumnos promedios 
-*/;
-create temporary table if not exists Tmp (idconsultas_horario int, idtiempo int , cant_alumnos int);
-insert into Tmp
-select c.idconsultas_horario, c.idtiempo ,count(idalumno) cant_alumnos
-from consultas c 
-join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
-join profesor p 
-		on ch.idprofesor=p.idprofesor
-where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
-group by c.idconsultas_horario, c.idtiempo ;
-select avg(cant_alumnos)
-from Tmp
-;
-
-*";
+        $sql = "
+        /*
+                    consultas ACEPTADAS
+        */
+        select count(*)
+        from consultas c 
+        join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+        join profesor p 
+                on c.idprofesor=p.idprofesor
+        join materia m 
+                on m.matriculaMateria=c.cod_materia
+        where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+        and nombre_materia= case when '$materia' is null then nombre_materia else '$materia' end
+        and upper(c.estado) ='ACEPTADA';
+        commit;
+        ";
+    case 5:
+        $classcss = "bg-gradient-info";
+        $title = "Alumnos Promedio";
+        $letter = "AP";
+        $sql = "
+        /*
+                    alumnos promedios 
+        */;
+        create temporary table if not exists Tmp (idconsultas_horario int, idtiempo int , cant_alumnos int);
+        insert into Tmp
+        select c.idconsultas_horario, c.idtiempo ,count(idalumno) cant_alumnos
+        from consultas c 
+        join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+        join profesor p 
+                on ch.idprofesor=p.idprofesor
+        where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+        group by c.idconsultas_horario, c.idtiempo ;
+        select avg(cant_alumnos)
+        from Tmp;
+        ";
         break;
   } 
 ?>
