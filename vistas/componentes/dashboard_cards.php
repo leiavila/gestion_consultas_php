36d37bf51d0e73bb9501ce7f07d9a231026432af
valentina <vale.venica@gmail.com>
2020-08-09 18:55:42 -0300

    agrego sql para que leilen las ordene

--- vistas/componentes/dashboard_cards.php
+++ vistas/componentes/dashboard_cards.php
@@ -1,24 +1,101 @@
 <?php
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+$resultado1 = $conexion->prepare("/*
+consultas pendientes
+*/
+select count(*)
+from consultas c 
+join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+join profesor p 
+      on c.idprofesor=p.idprofesor
+join materia m 
+      on m.matriculaMateria=c.cod_materia
+where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+and c.estado ='Pendiente';");
 switch ($cardnum) {
     case 1:
         $classcss = "bg-gradient-red";
         $title = "Consultas Pendientes";
         $letter = "P";
+        $sql = "/*
+              consultas pendientes
+            */
+            select count(*)
+            from consultas c 
+            join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+            join profesor p 
+                    on c.idprofesor=p.idprofesor
+            join materia m 
+                    on m.matriculaMateria=c.cod_materia
+            where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+            and c.estado ='Pendiente';
+            ";
+
         break;
     case 2:
         $classcss = "bg-gradient-orange";
         $title = "Consultas para hoy";
         $letter = "H";
+        $sql = "
+            /*
+                        consultas para hoy
+            */
+            select count(*)
+            from consultas c 
+            join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+            join profesor p 
+                    on ch.idprofesor=p.idprofesor
+            join tiempo t 
+                    on t.idtiempo=c.idtiempo
+            where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+            and t.fecha=current_date();
+            ";
+
         break;
     case 3:
         $classcss = "bg-gradient-green";
         $title = "Consultas canceladas";
         $letter = "C";
+        $sql = "
+        /*
+                    consultas Canceladas
+        */
+        select count(*)
+        from consultas c 
+        join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+        join profesor p 
+                on c.idprofesor=p.idprofesor
+        join materia m 
+                on m.matriculaMateria=c.cod_materia
+        where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+        and c.estado ='Cancelada';
+        commit;
+        ";
         break;
     case 4: 
         $classcss = "bg-gradient-info";
-        $title = "Consultas de la semana";
-        $letter = "S";
+        $title = "Alumnos promedio en consultas";
+        $letter = "A";
+
+$sql = "
+/*
+              alumnos promedios 
+*/;
+create temporary table if not exists Tmp (idconsultas_horario int, idtiempo int , cant_alumnos int);
+insert into Tmp
+select c.idconsultas_horario, c.idtiempo ,count(idalumno) cant_alumnos
+from consultas c 
+join consultas_horario ch on c.idconsultas_horario=ch.idconsultas_horario
+join profesor p 
+		on ch.idprofesor=p.idprofesor
+where nombre_profesor=case when '$profesor' is null then nombre_profesor else '$profesor' end
+group by c.idconsultas_horario, c.idtiempo ;
+select avg(cant_alumnos)
+from Tmp
+;
+
+*";
         break;
   } 
 ?>
