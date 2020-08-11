eaf088cc898323f97568b66cac1e499ecae837c4
valentina <vale.venica@gmail.com>
2020-08-10 21:23:56 -0300

    Agregado carga alumno

--- vistas/carga_alumno.php
+++ vistas/carga_alumno.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Gestor de consultas UTN</title>
+  <title>Datos Alumnos</title>
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700">
   <link rel="stylesheet" href="\bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="\gestion_consultas_php\estilos.css" type="text/css">
@@ -31,13 +31,13 @@
             <div class="card-header">
               <div class="row align-items-center">
                 <div class="col-8">
-                  <h3>Editar perfil</h3>
+                  <h3>Carga Clase de Consulta</h3>
                 </div>
               </div>
             </div>
             <div class="card-body">
               <form>
-                <h6 class="heading-small text-muted mb-4">Informacion Usuario</h6>
+                <h6 class="heading-small text-muted mb-4">Información Alumno</h6>
                 <div class="pl-lg-4">
                   <div class="row">
                     <div class="col-lg-12">
@@ -50,26 +50,31 @@
                   <div class="row">
                     <div class="col-lg-6">
                       <div class="form-group">
+                        <label class="form-control-label" for="input-legajo">Legajo</label>
+                        <input type="integer" id="input-legajo" class="form-control" placeholder="Ingrese Legajo">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-lg-6">
+                      <div class="form-group">
                         <label class="form-control-label" for="input-first-name">Nombre</label>
-                        <input type="text" id="input-first-name" class="form-control" placeholder="First name" value="Lucky">
+                        <input type="text" id="input-first-name" class="form-control" placeholder="First name" value="Nombre">
                       </div>
                     </div>
                     <div class="col-lg-6">
                       <div class="form-group">
                         <label class="form-control-label" for="input-last-name">Apellido</label>
-                        <input type="text" id="input-last-name" class="form-control" placeholder="Last name" value="Jesse">
+                        <input type="text" id="input-last-name" class="form-control" placeholder="Last name" value="Apellido">
                       </div>
                     </div>
                   </div>
                 </div>
-       
+                <button class="btn btn-primary btn-lg btn-block" type="submit">ANOTARME</button>
+                        
+                                               
+
                 
-                <div class="pl-lg-4">
-                  <div class="form-group">
-                    <label class="form-control-label">Observaciones</label>
-                    <textarea rows="4" class="form-control" placeholder="Ingrese observaciones"></textarea>
-                  </div>
-                </div>
               </form>
             </div>
           </div>
