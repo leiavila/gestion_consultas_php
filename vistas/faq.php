bcae70a08b3de1a17e7863db85e7303088067309
Pablo Ladreyt <pabloladreyt@gmail.com>
2020-09-13 02:18:04 -0300

    Fix validacion roles en preguntas frecuentes

--- vistas/faq.php
+++ vistas/faq.php
@@ -27,40 +27,47 @@
           <div class="card">
    
             <div class="card-body">
-                <div>
-                    <label><b>¿Cómo cancelo una consulta?</b></label>
-                    <p>Para cancelar una consulta se debe seleccionar de la menú izquierdo la opción “cancelación de consultas”. En la misma se podrá realizar dos acciones:</p>
-                    <ul>
-                        <li>Cancelación por día: Se selecciona un día y se cancelan las materias que se seleccionan</li>
-                        <li>Cancelación por semana: Se selecciona una semana y se cancelan todas las consultas de la misma</li>
-                    </ul>
-                    <p>Luego de completar el tipo de cancelación, se llenar el campo motivo de la cancelación y hacer click sobre "Cancelar consulta"</p>
-               
-                </div>
+              <?php 
+                if (isset($_SESSION["s_profesor"])) { 
+                  echo '<div>';
+                  echo '<label><b>¿Cómo cancelo una consulta?</b></label>';
+                  echo '<p>Para cancelar una consulta se debe seleccionar de la menú izquierdo la opción “cancelación de consultas”. En la misma se podrá realizar dos acciones:</p>';
+                  echo '<ul>';
+                 echo '     <li>Cancelación por día: Se selecciona un día y se cancelan las materias que se seleccionan</li>';
+                  echo '    <li>Cancelación por semana: Se selecciona una semana y se cancelan todas las consultas de la misma</li>';
+                  echo '</ul>';
+                  echo '<p>Luego de completar el tipo de cancelación, se llenar el campo motivo de la cancelación y hacer click sobre "Cancelar consulta"</p>';
+             
+              echo '</div>';
+                }
+                if (!isset($_SESSION["s_profesor"])) { 
+                  echo '<div>';
+                  echo '<label><b>¿Cómo actualizo las horas de consulta desde un Excel?</b></label>';
+                  echo '<p>Las horas deben estar cargadas en un Excel, como el adjuntado llamado “Excel_value”. Siempre se debe respetar su forma de carga. ';
+                  echo 'Una vez completo dicho Excel, se debe ingresar desde el menú izquierdo a la opción carga de horas. Desde “seleccionar archivo” levantar el excel y hacer click sobre el botón “Actualizar horas de consulta”. ';
+                 echo ' </p>';
+             echo ' </div>';
+                }
 
-                <div>
-                    <label><b>¿Cómo actualizo las horas de consulta desde un Excel?</b></label>
-                    <p>Las horas deben estar cargadas en un Excel, como el adjuntado llamado “Excel_value”. Siempre se debe respetar su forma de carga. 
-                    Una vez completo dicho Excel, se debe ingresar desde el menú izquierdo a la opción carga de horas. Desde “seleccionar archivo” levantar el excel y hacer click sobre el botón “Actualizar horas de consulta”. 
-                    </p>
-                </div>
+                if (isset($_SESSION["s_profesor"])) { 
+                echo '<div>';
+                    echo '<label><b>¿Cómo acepto o rechazo una consulta?</b></label>';
+                    echo '<p>Para aceptar una consulta seleccionar de la barra izquierda el sector “Listado de consulta” visualizar una consulta, en la parte derecha de la fila se encuentran dos botones: </p>';
+                   echo ' <ul>';
+                   echo '     <li>Aceptar</li>';
+                   echo '     <li>Rechazar</li>';
+                   echo ' </ul>';
+                   echo ' <p> Al seleccionar cada uno acepta o rechaza, respectivamente, una consulta pedida por un alimno </p>';
+                echo '</div>';
 
-                <div>
-                    <label><b>¿Cómo acepto o rechazo una consulta?</b></label>
-                    <p>Para aceptar una consulta seleccionar de la barra izquierda el sector “Listado de consulta” visualizar una consulta, en la parte derecha de la fila se encuentran dos botones: </p>
-                    <ul>
-                        <li>Aceptar</li>
-                        <li>Rechazar</li>
-                    </ul>
-                    <p> Al seleccionar cada uno acepta o rechaza, respectivamente, una consulta pedida por un alimno </p>
-                </div>
+               echo ' <div>';
+               echo '     <label><b>¿Qué pasa al aceptar o rechazar una consulta?</b></label>';
+               echo '     <p> Al aceptar o rechazar una consulta, se procede a enviar un mail al alumno que la solicito, informándole del estado de la consulta</p>';
+               echo ' </div>';
 
-                <div>
-                    <label><b>¿Qué pasa al aceptar o rechazar una consulta?</b></label>
-                    <p> Al aceptar o rechazar una consulta, se procede a enviar un mail al alumno que la solicito, informándole del estado de la consulta</p>
-                </div>
+              }
 
-
+              ?>
 
             </div>
     
