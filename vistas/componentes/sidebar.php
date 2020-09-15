34587b2c3b3b9a46499f4e52baf6d82bbd890b7f
Leilen <leilenavila@gmail.com>
2020-09-15 20:17:59 -0300

    sidebar ACTIVE

--- vistas/componentes/sidebar.php
+++ vistas/componentes/sidebar.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html> 
+<!DOCTYPE html>
+
 <head>
-<meta charset="utf-8">
+  <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Gestor de consultas UTN</title>
-  <link rel="stylesheet" href="..\..\estilos.css" type="text/css"> 
+  <link rel="stylesheet" href="..\..\estilos.css" type="text/css">
 </head>
 
 <body>
 
-<nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
+  <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div id="mySidenav" class="scrollbar-inner">
 
       <div class="sidenav-header  align-items-center">
@@ -17,69 +18,118 @@
         </a>
       </div>
       <div class="navbar-inner">
-       
+
         <div class="collapse navbar-collapse" id="sidenav-collapse-main">
-     
+
           <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="../vistas/dashboard.php">
-                <span class="nav-link-text">Menú principal</span>
-              </a>
-            </li>
             <?php
             session_start();
+            $page = $_SERVER['REQUEST_URI'];
+
+            echo '<li class="nav-item">';
+            echo ' <a class="nav-link ';
+            if (strpos($page, 'dashboard')) {
+              echo 'active';
+            }
+
+            echo '" href="../vistas/dashboard.php">
+                <span class="nav-link-text">Menú principal</span>
+              </a>
+            </li>';
+
+
             if (isset($_SESSION["s_profesor"])) {
-            echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../vistas/listado_consultas.php">';
-                echo '<span class="nav-link-text">Listado de consultas</span>';
-              echo '</a>';
-            echo '</li>';
 
-            echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../vistas/nueva_consulta.php">';
-                echo '<span class="nav-link-text">Nueva consulta</span>';
-              echo '</a>';
-            echo '</li>';
+              echo '<li class="nav-item ">';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'listado_consultas')) {
+                echo 'active';
+              }
 
-            echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../vistas/cancelacion_consultas.php">';
-                echo '<span class="nav-link-text">Cancelación de consultas</span>';
+              echo '" href="../vistas/listado_consultas.php">';
+              echo '<span class="nav-link-text">Listado de consultas</span>';
               echo '</a>';
-            echo '</li>';
+              echo '</li>';
+
+              echo '<li class="nav-item ">';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'nueva_consulta')) {
+                echo 'active';
+              }
+
+              echo '" href="../vistas/nueva_consulta.php">';
+              echo '<span class="nav-link-text">Nueva consulta</span>';
+              echo '</a>';
+              echo '</li>';
+
+              echo '<li class="nav-item">';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'cancelacion_consultas')) {
+                echo 'active';
+              }
+
+              echo '" href="../vistas/cancelacion_consultas.php">';
+              echo '<span class="nav-link-text">Cancelación de consultas</span>';
+              echo '</a>';
+              echo '</li>';
             }
             if (!isset($_SESSION["s_profesor"])) {
               echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../vistas/listado_consultas_admin.php">';
-                echo '<span class="nav-link-text">Listado de consultas</span>';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'listado_consultas_admin')) {
+                echo 'active';
+              }
+
+              echo '" href="../vistas/listado_consultas_admin.php">';
+              echo '<span class="nav-link-text">Listado de consultas</span>';
               echo '</a>';
-            echo '</li>';
+              echo '</li>';
+
               echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../upload-excel/uploadexc.php">';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'uploadexc')) {
+                echo 'active';
+              }
+              echo '" href="../upload-excel/uploadexc.php">';
               echo '<span class="nav-link-text">Carga de horas</span>';
               echo '</a>';
               echo '</li>';
             }
             if (isset($_SESSION["s_profesor"])) {
 
-            echo '<li class="nav-item">';
-              echo '<a class="nav-link" href="../vistas/mi_cuenta.php">';
-                echo '<span class="nav-link-text">Mi perfil</span>';
+              echo '<li class="nav-item">';
+              echo '<a class="nav-link ';
+              if (strpos($page, 'mi_cuenta')) {
+                echo 'active';
+              }
+
+              echo '" href="../vistas/mi_cuenta.php">';
+              echo '<span class="nav-link-text">Mi perfil</span>';
               echo '</a>';
-            echo '</li>';
-          }
+              echo '</li>';
+            }
 
             ?>
-            <li class="nav-item">
-              <a class="nav-link" href="../vistas/faq.php">
-                <span class="nav-link-text">Preguntas Frecuentes</span>
+            <?php
+            echo '<li class="nav-item">';
+            echo '<a class="nav-link ';
+            if (strpos($page, 'faq')) {
+              echo 'active';
+            }
+            echo '" href="../vistas/faq.php">';
+            echo ' <span class="nav-link-text">Preguntas Frecuentes</span>
               </a>
-            </li>
+            </li>';
+            ?>
 
             <li class="nav-item">
               <a class="nav-link" href="../bd/logout.php">
                 <span class="nav-link-text">Cerrar sesión</span>
               </a>
             </li>
+
+
+
           </ul>
         </div>
       </div>
