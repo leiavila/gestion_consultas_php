77a561e22bc7d850dc0e013514497cd2413718e2
Pablo Ladreyt <pabloladreyt@gmail.com>
2020-09-18 20:49:57 -0300

    Se muestran las consultas canceladas. Se fixea que no traiga canceladas en las proximas

--- vistas/dashboard.php
+++ vistas/dashboard.php
@@ -142,6 +142,99 @@
       </div>
 
     </div>
+    <br>
+
+    <div class="container-fluid mt--6">
+      <div class="row">
+        <div class="col-xl-12">
+          <div class="card">
+            <div class="card-header border-0">
+              <div class="row align-items-center">
+                <div class="col">
+                  <h3 class="mb-0">Consultas canceladas</h3>
+                </div>
+              </div>
+            </div>
+            <div class="table-responsive">
+
+              <table class="table align-items-center table-flush">
+                <thead class="thead-light">
+                  <tr>
+                    <th scope="col">Materia</th>
+                    <?php
+                    if (!isset($_SESSION["s_profesor"])) {
+                      echo '<th scope="col">Profesor</th>';
+                    }
+                    ?>
+                    <th scope="col">Fecha </th>
+                    <th scope="col">Hora inicio - fin</th>
+                    <th scope="col">Motivo</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <?php
+                  $Cant_por_Pag = 3;
+                  $resultado = $conexion->prepare('CALL consultas_canceladas(?, 0, 100000);');
+                  $resultado->execute([$_SESSION["s_profesor"]]);
+                  $pagina_canc = isset($_GET['pagina_canc']) ? $_GET['pagina_canc'] : null;
+                  if (!$pagina_canc) {
+                    $inicio = 0;
+                    $pagina_canc = 1;
+                  } else {
+                    $inicio = ($pagina_canc - 1) * $Cant_por_Pag;
+                  }
+                  $total_registros = $resultado->rowCount();
+                  $total_paginas_canc = ceil($total_registros / $Cant_por_Pag);
+                  $resultado = $conexion->prepare('CALL consultas_canceladas(?, ?, ?);');
+                  $resultado->execute([$_SESSION["s_profesor"], $inicio, $Cant_por_Pag]);
+                  $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
+
+                  if ($resultado->rowCount() > 0) {
+                    foreach ($data as $fila) {
+                      echo '<tr>';
+                      echo '<td><b>' . $fila["nombre_materia"] . '</b></td>';
+                      if (!isset($_SESSION["s_profesor"])) {
+                        echo '<td>' . $fila["nombre_profesor"] . '</td>';
+                      }
+
+                      $date = strtotime($fila["fecha_bloqueo"]);
+                      $new_date = date('d-m-Y', $date);
+                      echo '<td>' .   $new_date . '</td>';
+                      echo '<td>' . $fila["hora_ini"] . '</td>';
+                      echo '<td>' . $fila["motivo"] . '</td>';
+                      echo '</tr>';
+                    }
+                  } else {
+                    echo '<th colspan=4>No hay consultas canceladas recientemente.</th>';
+                  }
+                  ?>
+                </tbody>
+              </table>
+            </div>
+            <?php
+            if ($total_paginas_canc > 1) {
+              echo '<div class="card-footer py-4">';
+              echo '<nav aria-label="...">';
+              echo '<ul class="pagination justify-content-end mb-0">';
+
+              for ($i = 1; $i <= $total_paginas_canc; $i++) {
+
+                echo '<li class="page-item ';
+                echo ($pagina_canc == $i) ?  'active' : '';
+                echo '">';
+                echo '        <a class="page-link" href="dashboard.php?pagina_canc=' . $i . '">' . $i . '</a>';
+                echo '       </li>';
+              }
+              echo '    </ul>';
+              echo '  </nav>';
+              echo '</div>';
+            }
+            ?>
+          </div>
+        </div>
+      </div>
+
+    </div>
   </div>
   <script src="../assets/vendor/jquery/dist/jquery.min.js"></script>
   <script src="../assets/vendor/js-cookie/js.cookie.js"></script>
